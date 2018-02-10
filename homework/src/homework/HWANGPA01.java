7c0ca040dc3f71cc024b7061e699f75518d24789
wanghuo <huo@rams.colostate.edu>
2018-02-09 20:46:07 -0700

    commit

--- homework/src/homework/HWANGPA01.java
+++ homework/src/homework/HWANGPA01.java
@@ -1,5 +1,23 @@
 package homework;
 
-public class HWANGPA01 {
+import javax.swing.JOptionPane;
 
+public class HWANGPA01 {
+	public static void main(String[]args)
+	{
+	
+		{
+			JOptionPane.showMessageDialog(null, "Your Full name as: First name, Last name");
+			String name = JOptionPane.showInputDialog("Your name");
+			name = "name:" + name;
+			 JOptionPane.showMessageDialog(null, name);
+			JOptionPane.showMessageDialog(null, "Please enter your City and Country of birth"); 
+			String city = JOptionPane.showInputDialog("City of Birth");
+			JOptionPane.showMessageDialog(null, "city:" + city);
+			String back = JOptionPane.showInputDialog("Please enter your academic background");
+			JOptionPane.showMessageDialog(null, "academic background:"+back);
+			String fun = JOptionPane.showInputDialog("Please enter something about yourself,"+"it can be information or a fun fact, fun facts may be shared with the class in the lecture");
+			JOptionPane.showMessageDialog(null, fun);
+		}
+	}
 }
--- homework/src/homework/TaxCalculation.java
+++ homework/src/homework/TaxCalculation.java
@@ -1,5 +1,104 @@
 package homework;
 
+import javax.swing.JOptionPane;
+
 public class TaxCalculation {
 
-}
+		public static void main(String[]args) 
+		{
+		  double T = 0 ;
+		  boolean loop=true;
+		  while(loop) 
+			{
+			JOptionPane.showMessageDialog(null," enter how many people you want?");//input people
+			String num = JOptionPane.showInputDialog("number of people:");
+			int n = Integer.parseInt(num);
+			for(int x = 1; n >= x ;x++) // for loop (N people input)
+			{
+				JOptionPane.showMessageDialog(null, "which status? single/married"); //output status
+				String status = JOptionPane.showInputDialog("Your status(0 for single/1 for married)");
+				int a = Integer.parseInt(status);// define status
+				switch (a)
+				{
+				// single tax
+				case 0:
+				{
+					JOptionPane.showMessageDialog(null, "enter your income");
+				    String income = JOptionPane.showInputDialog("Your income:");
+				    double i = Double.parseDouble(income);
+				    if ( i < 8351)
+					{
+					    T = i* 0.1;
+					}
+					else if ( i < 33951 )
+					{
+					    T = ( i - 8350 )*0.15+8350*0.1;
+					}
+					else if ( i<82251 )
+					{
+					    T = (i- 33950)*0.25+(33950-8350)*0.15+8350*0.1;
+					}
+					else if ( i<171551 )
+					{
+					    T = (i- 82250)*0.28+(82250- 33950)*0.25+(33950-8350)*0.15+8350*0.1;
+					}
+					else if ( i<372951 )
+					{
+					    T =(i-171550)*0.33+ (171550- 82250)*0.28+(82250- 33950)*0.25+(33950-8350)*0.15+8350*0.1;
+					}
+					else if (i>=372951)
+					{
+					    T =(i-372950)*0.35+(372950-171550)*0.33+ (171550- 82250)*0.28+(82250- 33950)*0.25+(33950-8350)*0.15+8350*0.1;
+					}
+				    JOptionPane.showMessageDialog(null, "income="+ income + "\nStatus(0:single;1:married)="+ status + "\nTAX="+T);
+				}
+				
+				loop= false;
+				  break;
+				  // married tax
+				  case 1:
+				  {
+				JOptionPane.showMessageDialog(null, "enter your income");
+				String income = JOptionPane.showInputDialog("Your income");
+				double i = Double.parseDouble(income);
+				if (i <16701)
+				{
+				T = i* 0.1;
+				}
+				else if ( i<67901 )
+				{
+				T = (i-16700)*0.15+16700*0.1;
+				}
+				else if ( i<137051 )
+				{
+				T = (i- 67900)*0.25+(67900-16700)*0.15+16700*0.1;
+				}
+				else if ( i<208851 )
+				{
+				T = (i- 137050)*0.28+(137050- 67900)*0.25+(67900-16700)*0.15+16700*0.1;
+				}
+				else if ( i<372951 )
+				{
+		 		T =(i-208850)*0.33+ (208850- 137050)*0.28+(137050- 67900)*0.25+( 67900-16700)*0.15+16700*0.1;
+				}
+				else if (i>=372951)
+				{
+				T =(i-372950)*0.35+(i-208850)*0.33+ (208850- 137050)*0.28+(137050- 67900)*0.25+( 67900-16700)*0.15+16700*0.1;
+				}
+				JOptionPane.showMessageDialog(null, "income="+ income + "\nStatus(0:single;1:married)="+ status + "\nTAX="+T);
+			     }
+				  
+				loop = false;
+				  break;
+				  // except wrong number for status.
+				  default:
+				  {
+					JOptionPane.showMessageDialog(null, "enter 0 (for single)or 1(for married)");
+				loop = true;
+				  }
+				}
+			 } 
+			}
+		}
+	}
+
